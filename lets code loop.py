78f9c11f1f3e7b63f5fd5686a8ed6c3b7be372df
kushalsabe <86290871+kushalsabe@users.noreply.github.com>
2021-06-30 19:52:00 +0530

    Add files via upload

---
+++ lets code loop.py
@@ -0,0 +1,10 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3] 
+
+pos_nos = [num for num in list1 if num >= 0]
+  
+print("Positive numbers in the list: ", *pos_nos)
+  
+pos_nop = [num for num in list2 if num >= 0]
+  
+print("Positive numbers in the list: ", *pos_nop)
